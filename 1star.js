27ae46f27a31a59e980d2ebe252a4e53de50e3a5
Viktoryia0106 <serafinko36@mail.ru>
2022-07-03 01:42:20 +0300

    modified

--- 1star.js
+++ 1star.js
@@ -1,10 +1,4 @@
-
-// Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
-// Пример: const checkAge = function(age) {
-// Ваши преобразования
-// }
-// Вывести в консоль результат работы функции с возрастами 17, 18, 61
-
+// 1 part
 const age_2 = 18;
 const age_3 = 60;
 
@@ -23,3 +17,51 @@ if (age < age_2) {
 checkAge(17)
 checkAge(18)    
 checkAge(61)
+
+// 2 part
+ {
+   age = Number(age)
+   console.log(typeof(age))
+if(age) { 
+    if (age < age_2) {
+   console.log("You don’t have access cause your age is" + age + "It’s less then")
+} else if (age >= age_2 && age < age_3) {
+   console.log("Welcome !")
+} else if (age > age_3) { 
+   console.log("Keep calm and look Culture channel")
+} else {
+   console.log("Technical work")
+  }
+} else console.log('Not an integer value')
+}
+checkAge(17)
+checkAge(18)    
+checkAge(60)
+checkAge(61)
+checkAge("privet")
+checkAge("33")
+checkAge('0')
+checkAge('null')
+
+// 3 part
+ {
+   if(age && !isNaN(age)) {
+     if (age < age_2) {
+        console.log("You don’t have access cause your age is" + age + "It’s less then");
+     } else if (age >= age_2 && age < age_3) {
+        console.log("Welcome !");
+     } else if (age > age_3) { 
+        console.log("Keep calm and look Culture channel");
+     } else {
+        console.log("Technical work");
+     }
+   } else console.log('Not an integer value')
+   }
+   checkAge(17)
+   checkAge(18)    
+   checkAge(60)
+   checkAge(61)
+   checkAge('aaa')
+   checkAge('33')
+   checkAge('0')
+   checkAge('')
