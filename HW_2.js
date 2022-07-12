4c5d2bea8da8e000ab0c739a8116ff4c117ee25d
Viktoryia0106 <serafinko36@mail.ru>
2022-07-13 00:15:38 +0300

    modified hw

--- HW_2.js
+++ HW_2.js
@@ -1,4 +1,4 @@
-// 1 task
+// 1. Написать скриптик, который сосчитает и выведет результат от возведения 2 в степень 10, начиная со степени 1
 
 for(let i = 1; i <11; i++) {
 console.log(2 ** i )
@@ -8,7 +8,7 @@ for(let i = 1; i <11; i++) {
     console.log(Math.pow(2,i) )
     }
 
- // 1* task
+ // 1*. Преобразовать 1 задачу в функцию, принимающую на вход степень, в которую будет возводиться число 2
 
 const pow = function(counter) {
     for(let i = 1; i <=counter; i++) {
@@ -17,7 +17,13 @@ const pow = function(counter) {
 }
 pow(10)
 
-// 2 task
+// 2. Написать скрипт, который выведет 5 строк в консоль таким образом, чтобы в первой строчке выводилось :), во второй :):) и так далее
+// Пример в консоли:
+// :)
+// :):)
+// :):):)
+// :):):):)
+// :):):):):)
 
 let result = ''
 let smile = ';)'
@@ -29,7 +35,8 @@ for(let i = 1; i<6; i++) {
     console.log(smile.repeat(i) ) 
 }
 
-// 2* task
+// 2*. Преобразовать 2 задачу в функцию, принимающую на вход строку, которая и будет выводиться в консоль (как в условии смайлик), а также количество строк для вывода 
+// e.g. function printSmile(stroka, numberOfRows)
 
 function smilik(str, numberOfRows) {
 for(let i = 1; i<=numberOfRows; i++) {
@@ -40,7 +47,11 @@ for(let i = 1; i<=numberOfRows; i++) {
 
 smilik(':(', 10)
 
-// 3** task
+// 3**.  Написать функцию, которая принимает на вход слово. Задача функции посчитать и вывести в консоль, сколько в слове гласных, и сколько согласных букв.
+// e.g. function getWordStructure(word)
+// В консоли: 
+// Слово (word) состоит из  (число) гласных и (число) согласных букв
+// Проверки: 'case', 'Case', 'Check-list'
 
 function getWordStructure(word) {
     const vowels = 'aeiouy'.split('')
@@ -59,7 +70,9 @@ getWordStructure("case")
 getWordStructure("Case")
 getWordStructure("Check-list")
 
-// 4** task
+// 4**. Написать функцию, которая проверяет, является ли слово палиндромом
+// e.g. function isPalindrom(word)
+// Проверки: 'abba', 'Abba'
 
 function isPalindrom(word) {
     word = word.toLowerCase()
